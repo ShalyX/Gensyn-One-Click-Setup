96640216459f1a05300f07c6f186cbf51c591690
ShalyX <131195534+ShalyX@users.noreply.github.com>
2025-11-05 20:08:45 +0100

    Update setup_gensyn_node.sh

--- setup_gensyn_node.sh
+++ setup_gensyn_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # --- Configuration ---
-REPO_URL="https://github.com/gensyn/rl-swarm.git"  # Official Gensyn repo (confirm this URL is correct before final deployment)
+REPO_URL="git@github.com:gensyn/rl-swarm.git"  # Official Gensyn repo (confirm this URL is correct before final deployment)
 REPO_DIR="rl-swarm"
 SCREEN_NAME="gensyn"
 
